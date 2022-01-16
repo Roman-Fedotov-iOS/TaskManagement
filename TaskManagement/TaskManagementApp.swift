d414d53fb8007cb9757c98c5255df1038a3dd6f1
Roman-Fedotov-iOS <romafed23@gmail.com>
2022-01-16 15:50:50 +0200

    Initial Commit

---
+++ TaskManagement/TaskManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagementApp.swift
+//  TaskManagement
+//
+//  Created by Roman Fedotov on 16.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
